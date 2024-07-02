86bd17ccdd6e5e61a594013d6d7ef66eb502087a
Dishank-shah25 <dishankshah2000@gmail.com>
2024-07-02 18:38:49 +0530

    Update settings.py

--- newproject/settings.py
+++ newproject/settings.py
@@ -80,16 +80,16 @@ WSGI_APPLICATION = 'newproject.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'angulardata',
-        'USER': 'postgres',
-        'PASSWORD': 'dishank',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'angulardata',
+#         'USER': 'postgres',
+#         'PASSWORD': 'dishank',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
 
 
 # Password validation
