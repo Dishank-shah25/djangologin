b9e4054285e8b006c6cdba1deaa273bde9f82a6a
Dishank-shah25 <dishankshah2000@gmail.com>
2024-07-04 16:28:33 +0530

    Update urls.py

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,9 +1,10 @@
 # newapp\urls.py
 from django.urls import path
-from .views import create_user , login_user , get_all_users
+from .views import create_user , login_user , get_all_users , AngularAppView
 
 urlpatterns = [
     path('create-user/', create_user, name='create_user'),
     path('login/', login_user),
-    path('', get_all_users, name='get_all_users'),
+    path('get_all_users', get_all_users, name='get_all_users'),
+     path('', AngularAppView.as_view(), name='angular-app'),
 ]
