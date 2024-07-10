8b18beebc9f257b20119987889ad738154045b09
Dishank-shah25 <dishankshah2000@gmail.com>
2024-07-10 18:49:53 +0530

    Update urls.py

--- newapp/urls.py
+++ newapp/urls.py
@@ -1,6 +1,6 @@
 # newapp\urls.py
 from django.urls import path
-from .views import create_user , login_user , get_all_users , AngularAppView
+from .views import create_user , login_user , get_all_users
 
 urlpatterns = [
     path('create-user/', create_user, name='create_user'),
